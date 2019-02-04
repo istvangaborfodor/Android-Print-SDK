b37b6a3827af1030d57b61cc50e10e7ca717818d
Istvan Gabor Fodor <istvan@fodor.work>
2019-02-04 16:23:38 +0100

    generate key
    generate key directly into the secure key store

--- KitePrintSDK/src/main/java/ly/kite/SecurePreferences.java
+++ KitePrintSDK/src/main/java/ly/kite/SecurePreferences.java
@@ -21,18 +21,34 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+import android.content.Context;
+import android.os.Build;
+import android.security.KeyPairGeneratorSpec;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import android.support.annotation.RequiresApi;
 import android.util.Base64;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.security.GeneralSecurityException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableEntryException;
+import java.security.cert.CertificateException;
 
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import javax.security.auth.x500.X500Principal;
 
 
 public class SecurePreferences {
@@ -44,6 +60,10 @@ public class SecurePreferences {
         }
 
     }
+
+    private static final String PROVIDER = "AndroidKeyStore";
+    private static final String ALIAS = "securePreferences";
+    private static final String M_TRANSFORMATION = "AES/CBC/PKCS7Padding";
 
     private static final String TRANSFORMATION = "AES/CBC/PKCS5Padding";
     private static final String SECRET_KEY_HASH_TRANSFORMATION = "SHA-256";
@@ -57,17 +77,32 @@ public class SecurePreferences {
 
     /**
      * This will initialize an instance of the SecurePreferences class
+     *
      * @param secureKey the key used for encryption, finding a good key scheme is hard.
-     * Hardcoding your key in the application is bad, but better than plaintext preferences. Having the user enter the key upon application launch is a safe(r) alternative, but annoying to the user.
-     * true will encrypt both values and keys. Keys can contain a lot of information about
-     * the plaintext value of the value which can be used to decipher the value.
+     *                  Hardcoding your key in the application is bad, but better than plaintext preferences. Having the user enter the key upon application launch is a safe(r) alternative, but annoying to the user.
+     *                  true will encrypt both values and keys. Keys can contain a lot of information about
+     *                  the plaintext value of the value which can be used to decipher the value.
      * @throws SecurePreferencesException
      */
     public SecurePreferences(String secureKey) throws SecurePreferencesException {
-        if(secureKey.toLowerCase().equals("off"))
+        if (KiteSDK.ENCRYPTION_KEY.equals("off"))
             encryptData = false;
 
-        this.secureKey = secureKey;
+        //TODO migrate data encrypted with legacy key
+        String tempKey = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            try {
+                tempKey = Base64.encodeToString(getSecureKey().getEncoded(), Base64.DEFAULT);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(tempKey != null)
+            this.secureKey = tempKey;
+        else
+            this.secureKey = secureKey;
+
         reset();
     }
 
@@ -84,7 +119,7 @@ public class SecurePreferences {
     }
 
     protected void initCiphers(String secureKey) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException,
-            InvalidAlgorithmParameterException {
+        InvalidAlgorithmParameterException {
         IvParameterSpec ivSpec = getIv();
         SecretKeySpec secretKey = getSecretKey(secureKey);
 
@@ -112,7 +147,7 @@ public class SecurePreferences {
 
 
     public String encrypt(String value) throws SecurePreferencesException {
-        if(encryptData == false)
+        if (encryptData == false)
             return value;
         else {
             if (value == null)
@@ -129,7 +164,7 @@ public class SecurePreferences {
     }
 
     public String decrypt(String securedEncodedValue) {
-        if(encryptData==false)
+        if (encryptData == false)
             return securedEncodedValue;
         else {
             if (securedEncodedValue == null)
@@ -147,9 +182,35 @@ public class SecurePreferences {
     private static byte[] convert(Cipher cipher, byte[] bs) throws SecurePreferencesException {
         try {
             return cipher.doFinal(bs);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             throw new SecurePreferencesException(e);
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private SecretKey getSecureKey() throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableEntryException, NoSuchProviderException, InvalidAlgorithmParameterException {
+        // key retrieval
+        KeyStore keyStore = KeyStore.getInstance(PROVIDER);
+        keyStore.load(null);
+
+        KeyStore.SecretKeyEntry entry = (KeyStore.SecretKeyEntry) keyStore.getEntry(ALIAS, null);
+        if (entry != null) {
+            return entry.getSecretKey();
+        } else {
+            // key generation
+            KeyGenParameterSpec.Builder builder = new KeyGenParameterSpec.Builder(ALIAS,
+                KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT);
+
+            KeyGenParameterSpec keySpec = builder
+                .setKeySize(128)
+                .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                .build();
+
+            KeyGenerator kg = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, PROVIDER);
+            kg.init(keySpec);
+            return kg.generateKey();
         }
     }
 }
